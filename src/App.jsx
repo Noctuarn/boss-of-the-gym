14db5d62396b1b062108ae238515c1b19a091d8b
Roman Humenyak <romangumenyak2003@gmail.com>
2023-07-22 18:14:51 +0300

    create NavBar's markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 import ExerciseDetail from './pages/ExerciseDetail';
 import Home from "./pages/Home"
 import Navbar from "./components/Navbar"
+import Footer from './components/Footer';
 
 import './App.scss';
 
 function App() {
   return (
     <>
-      <Box width="400px">
+      <Box width="400px" sx={{width: {xl: "1448px"}}} m="auto">
         <Navbar/>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/exercise/:id' element={<ExerciseDetail />} />
         </Routes>
+        <Footer/>
       </Box>
     </>
   );
