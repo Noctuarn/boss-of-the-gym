bfb0db7de280a72f9d4663cb7849bfcbbae75db3
Roman Humenyak <romangumenyak2003@gmail.com>
2023-07-25 18:35:41 +0300

    create footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Stack, Box, Typography } from '@mui/material'
+
+import Logo from "../assets/images/Logo-1.png"
 
 const Footer = () => {
   return (
-    <div>
-       Hello from Footer
-    </div>
+    <Box mt={"80px"} bgcolor={"#fff3f4"}>
+      <Stack gap={"40px"} alignItems={"center"} px={"40px"} py={"24px"}>
+        <img src={Logo} alt="" width={"200px"} height={"48px"} />
+      </Stack>
+    </Box>
   )
 }
 
--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -7,6 +7,7 @@ import ExerciseVideos from "../components/ExerciseVideos";
 import Detail from "../components/Detail";
 
 
+
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
   const [exerciseVideos, setexerciseVideos] = useState([]);
